86b38b5dad739afbe8ebaf4700e7ebc207379fd7
DioenDJS <negociospacheti@gmail.com>
2021-06-03 12:30:34 -0300

    codificando o componente Player

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -2,6 +2,8 @@ import styles from './styles.module.scss';
 
 export default function Player(){
     return(
-        <h1>PLAYER</h1>
+        <div className={styles.playerContainer}>
+
+        </div>
     )
 }
